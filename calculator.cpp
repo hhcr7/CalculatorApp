445b0cf13649800b8c6019bd4759175946c0c08f
Himanshu Hegde <86446705+hhcr7@users.noreply.github.com>
2022-01-08 22:20:51 -0600

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,136 @@
+#include "calculator.h"
+#include "ui_calculator.h"
+#include <QRegularExpression>
+
+double calcVal = 0.0;
+bool divTrigger= false;
+bool multTrigger= false;
+bool addTrigger= false;
+bool subTrigger= false;
+bool sinTrigger = false;
+bool cosTrigger = false;
+bool tanTrigger = false;
+
+Calculator::Calculator(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::Calculator)
+{
+    ui->setupUi(this);
+
+    ui->Display->setText(QString::number(calcVal));
+    QPushButton *numButtons[10];
+    for(int i=0;i<10;i++){
+        QString butName = "Button" + QString::number(i);
+        numButtons[i] = Calculator::findChild<QPushButton *>(butName);
+        connect(numButtons[i],SIGNAL(released()), this, SLOT(NumPressed()));
+
+    }
+
+    connect(ui->Add,SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->Subtract,SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->Multiply,SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->Divide,SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->sin,SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->cos,SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->tan,SIGNAL(released()), this, SLOT(MathButtonPressed()));
+    connect(ui->Equals,SIGNAL(released()), this, SLOT(EqualButtonPressed()));
+    connect(ui->ChangeSign,SIGNAL(released()), this, SLOT(ChangeNumberSign()));
+    connect(ui->Clear,SIGNAL(released()), this, SLOT(ClearButtonPressed()));
+
+}
+
+Calculator::~Calculator()
+{
+    delete ui;
+}
+
+void Calculator::NumPressed(){
+    QPushButton *button = (QPushButton *)sender();
+    QString butVal = button->text();
+    QString displayVal =ui->Display->text();
+    if((displayVal.toDouble()==0) || (displayVal.toDouble()==0.0)){
+        ui->Display->setText(butVal);
+    } else{
+        QString newVal = displayVal + butVal;
+        double dblNewVal = newVal.toDouble();
+        ui->Display->setText(QString::number(dblNewVal,'g',16));
+    }
+}
+
+void Calculator:: MathButtonPressed(){
+    divTrigger = false;
+    multTrigger = false;
+    addTrigger = false;
+    subTrigger = false;
+    sinTrigger = false;
+    cosTrigger = false;
+    tanTrigger = false;
+    QString displayVal = ui->Display->text();
+    calcVal = displayVal.toDouble();
+    QPushButton *button = (QPushButton *)sender();
+    QString butVal = button->text();
+    if(QString::compare(butVal,"/",Qt::CaseInsensitive)==0){
+        divTrigger=true;
+    } else if(QString::compare(butVal,"*",Qt::CaseInsensitive)==0){
+        multTrigger=true;
+    } else if(QString::compare(butVal,"+",Qt::CaseInsensitive)==0){
+        addTrigger=true;
+    }else if(QString::compare(butVal,"-",Qt::CaseInsensitive)==0){
+        subTrigger=true;
+    }else if(QString::compare(butVal,"sin",Qt::CaseInsensitive)==0){
+        sinTrigger=true;
+    }else if(QString::compare(butVal,"cos",Qt::CaseInsensitive)==0){
+        cosTrigger=true;
+    }else{
+        tanTrigger=true;
+    }
+    ui->Display->setText("");
+}
+
+void Calculator::EqualButtonPressed(){
+    double solution = 0.0;
+    QString displayVal=ui->Display->text();
+    double dblDisplayVal = displayVal.toDouble();
+    if(addTrigger || subTrigger || multTrigger || divTrigger || sinTrigger || cosTrigger || tanTrigger){
+        if(addTrigger){
+            solution=calcVal+dblDisplayVal;
+        } else if(subTrigger){
+            solution=calcVal-dblDisplayVal;
+        } else if(multTrigger){
+            solution=calcVal*dblDisplayVal;
+        }else if(divTrigger){
+            solution=calcVal/dblDisplayVal;
+        }else if(sinTrigger){
+            solution=sin(calcVal);
+        }else if(cosTrigger){
+            solution=cos(calcVal);
+        }
+        else{
+            solution=tan(calcVal);
+        }
+    }
+    ui -> Display -> setText(QString::number(solution));
+}
+
+void Calculator::ChangeNumberSign(){
+    QString displayVal = ui->Display->text();
+    QRegularExpression reg("[-]?[0-9.]*");
+
+    QRegularExpressionMatch match = reg.match(displayVal);
+        if(match.hasMatch()) {
+            double dblDisplayVal = displayVal.toDouble();
+            double dblDisplayValSign = -1 * dblDisplayVal;
+            ui->Display->setText(QString::number(dblDisplayValSign));
+        }
+    /*
+    if(reg.exactMatch(displayVal)){
+        double dblDisplayVal = displayVal.toDouble();
+        double dblDisplayValSign = -1*dblDisplayVal;
+        ui->Display->setText(QString::number(dblDisplayValSign));
+    }
+    */
+}
+
+void Calculator:: ClearButtonPressed(){
+    ui->Display->setText("0");
+}
